8c480e9d2790781966f7e4a49f3504216a093b03
“xyrccc” <793637465@qq.com>
2020-01-14 16:29:25 +0800

    筛选

--- src/components/Table/Table2.vue
+++ src/components/Table/Table2.vue
@@ -5,56 +5,54 @@
       表格2
     </div>
 
-    <el-table :data="list" border :summary-method="getSummaries" show-summary style="width: 100%;" stripe height="260">
-      <el-table-column label="序号" width="80px" align='center'>
+    <el-table :data="data" style="width: 100%;" stripe height="360">
+      <el-table-column prop="exchangeName" label="交易所名称" align='center'>
         <template slot-scope="scope">
-          <span>{{ scope.$index +1 }}</span>
+          <span v-if="!scope.row.isEgdit">{{scope.row.exchangeName}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.exchangeName"></el-input>
         </template>
       </el-table-column>
-      <el-table-column prop="firstNum" label="币种" align='center'>
+      <el-table-column prop="symbol" label="币种" align='center'>
         <template slot-scope="scope">
-          <span v-if="!scope.row.isEgdit">{{scope.row.firstNum}}</span>
-          <el-input v-if="scope.row.isEgdit" v-model="scope.row.firstNum"></el-input>
+          <span v-if="!scope.row.isEgdit">{{scope.row.symbol}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.symbol"></el-input>
         </template>
       </el-table-column>
-      <el-table-column prop="firstNum" label="初始" align='center'>
+      <el-table-column prop="asset" label="资产名" align='center'
+                       :filters="[{ text: 'USDT', value: 'USDT' }, { text: 'WDC', value: 'WDC' }]"
+                       :filter-method="filterAsset"
+                       filter-placement="bottom-end">
         <template slot-scope="scope">
-          <span v-if="!scope.row.isEgdit">{{scope.row.firstNum}}</span>
-          <el-input v-if="scope.row.isEgdit" v-model="scope.row.firstNum"></el-input>
+          <span v-if="!scope.row.isEgdit">{{scope.row.asset}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.asset"></el-input>
         </template>
       </el-table-column>
-      <el-table-column prop="secondNum" label="公元2019年12月22日" align='center'>
+      <el-table-column prop="all" label="总数" align='center'>
         <template slot-scope="scope">
-          <span v-if="!scope.row.isEgdit">{{scope.row.secondNum}}</span>
-          <el-input v-if="scope.row.isEgdit" v-model="scope.row.secondNum"></el-input>
+          <span v-if="!scope.row.isEgdit">{{scope.row.all}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.all"></el-input>
         </template>
       </el-table-column>
-      <el-table-column prop="thirdNum" label="12月27日-配平后" align='center'>
+      <el-table-column prop="available" label="可用" align='center'>
         <template slot-scope="scope">
-          <span v-if="!scope.row.isEgdit">{{scope.row.thirdNum}}</span>
-          <el-input v-if="scope.row.isEgdit" v-model="scope.row.thirdNum"></el-input>
+          <span v-if="!scope.row.isEgdit">{{scope.row.available}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.available"></el-input>
         </template>
       </el-table-column>
-      <el-table-column prop="fourthNum" label="公元2020年1月7日" align='center'>
+      <el-table-column prop="frozen" label="冻结" align='center'>
         <template slot-scope="scope">
-          <span v-if="!scope.row.isEgdit">{{scope.row.fourthNum}}</span>
-          <el-input v-if="scope.row.isEgdit" v-model="scope.row.fourthNum"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column prop="fourthNum" label="01月07日-配平后" align='center'>
-        <template slot-scope="scope">
-          <span v-if="!scope.row.isEgdit">{{scope.row.fourthNum}}</span>
-          <el-input v-if="scope.row.isEgdit" v-model="scope.row.fourthNum"></el-input>
+          <span v-if="!scope.row.isEgdit">{{scope.row.frozen}}</span>
+          <el-input v-if="scope.row.isEgdit" v-model="scope.row.frozen"></el-input>
         </template>
       </el-table-column>
       <el-table-column fixed="right" label="操作" align='center'>
         <template slot-scope="scope">
           <el-button v-if="!scope.row.isEgdit" type="primary" size="small" @click='edit(scope.$index,scope.row)' icon="el-icon-edit" circle></el-button>
           <el-button v-if="scope.row.isEgdit" type="success" size="small" @click='editSuccess(scope.$index,scope.row)' icon="el-icon-check" circle></el-button>
-          <el-button @click.native.prevent="deleteItem(scope.$index, list)" type="danger" size="small" icon="el-icon-delete" circle></el-button>
         </template>
       </el-table-column>
     </el-table>
+    <br>{{data}}
 
   </div>
 </template>
@@ -62,42 +60,44 @@
     export default {
         data() {
             return {
-                list: [{
-                    id: 1,
-                    fruitSort: 1,
-                    firstNum: 10,
-                    secondNum: 3,
-                    thirdNum: 4,
-                    fourthNum: 6,
-                }, {
-                    id: 2,
-                    fruitSort: 2,
-                    firstNum: 7,
-                    secondNum: 6,
-                    thirdNum: 8,
-                    fourthNum: 2,
-                }, {
-                    id: 3,
-                    fruitSort: 3,
-                    firstNum: 5,
-                    secondNum: 6,
-                    thirdNum: 8,
-                    fourthNum: 9,
-                }, {
-                    id: 4,
-                    fruitSort: 3,
-                    firstNum: 10,
-                    secondNum: 3,
-                    thirdNum: 4,
-                    fourthNum: 6,
-                }],
+                code:'',
+                message:'',
+                data:[],
+                total:'',
+                size:'',
             }
         },
+
+        mounted(){
+            let jsonObj={
+                "code": 200,
+                "message": "OK",
+                "data": [
+                    {
+                        "id": 48,
+                        "exchangeName": "abit",
+                        "symbol": "wdc_usdt",
+                        "asset": "USDT",
+                        "all": 299999.99999999,
+                        "available": 292106.43078544,
+                        "frozen": 7893.56921455
+                    },
+                    {
+                        "id": 49,
+                        "exchangeName": "abit",
+                        "symbol": "wdc_usdt",
+                        "asset": "WDC",
+                        "all": 2000000.00003263,
+                        "available": 1600259.86815647,
+                        "frozen": 399740.13187616
+                    }
+                ],
+                "total": 2,
+                "size": 2
+            }
+            this.data=jsonObj.data;
+        },
         methods: {
-            //删除数据
-            deleteItem(index, list) {
-                list.splice(index, 1);
-            },
             //编辑数据
             edit(index, row) {
                 this.$set(row, 'isEgdit', true)
@@ -105,8 +105,12 @@
             //编辑成功
             editSuccess(index, row) {
                 this.$set(row, 'isEgdit', false)
+                console.log(index)
+                console.log(row)
             },
-
+            filterAsset(value,row){
+                return row.asset === value;
+            }
         }
 
     }
